3a0d7ff7c8c6ec16edcb20a49a1a3a84cb82a7d4
wb-shl391697 <wb-shl391697@alibaba-inc.com>
2020-12-28 22:43:17 +0800

    feat code better

--- bin/react-cli.js
+++ bin/react-cli.js
@@ -3,19 +3,16 @@ const { resolve } = require("path");
 const res = (command) => resolve(__dirname, "../commands/", command);
 const program = require("commander");
 
-console.log(1);
-
 program.version(require("../package").version);
 
 program.usage("<command>");
-console.log(2);
 
 program
   .command("init")
-  .option("-f, --foo", "enable some foo")
   .description("Generate a new project")
   .alias("i")
   .action(() => {
-    console.log(3);
     require(res("init"));
   });
+
+program.parse(process.argv);
